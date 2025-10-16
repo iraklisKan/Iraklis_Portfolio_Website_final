1f68a9a36d6da864a126faa5dd3f774737f092e2
Iraklis Kanarkotis <ikanarkotis@aeon.local>
2025-10-16 22:01:26 +0300

    Remove 'reach out to me' text from footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,11 +21,7 @@ const Footer = () => {
           Ready to take <span className="text-purple">your</span> digital
           presence to the next level?
         </h1>
-        <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let&apos;s discuss how I can help you
-          achieve your goals.
-        </p>
-        <a href="mailto:irakliskanbusiness@gmail.com">
+        <a href="mailto:irakliskanbusiness@gmail.com" className="mt-10">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
